de0031b7531977fe9f6b320d094d6f480cecd1d7
Reyso Teixeira <57074420+Reyso@users.noreply.github.com>
2025-03-07 19:20:59 -0300

    Update app.py

--- MM/app.py
+++ MM/app.py
@@ -130,7 +130,7 @@ def main():
         progress_text = "Pedindo ajuda dos universitários..."
         my_bar = st.progress(0, text=progress_text)
 
-        for percent_complete in range(100):
+        for percent_complete in range(200):
             time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
         time.sleep(1)
